79ab0778d51802244d622d9911aaa9d95ad2e42a
macbook <macbook@macbooks-MacBook-Pro.local>
2024-12-02 07:33:40 +0700

    update price3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createGtm } from "@gtm-support/vue-gtm";
 
 // สร้างแอปพลิเคชันและใช้ router
-createApp(App).use(router).mount("#app");
+createApp(App)
+  .use(
+    createGtm({
+      id: "GTM-MLR855KP", // GTM ID // form-register-vbac
+    })
+  )
+  .use(router)
+  .mount("#app");
