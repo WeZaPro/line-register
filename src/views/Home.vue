eb0a0c9bf35ea796f1a50ff658bd25dddb18c04a
macbook <macbook@macbooks-MacBook-Pro.local>
2025-01-03 12:10:23 +0700

    update1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,6 +19,7 @@
 </template>
 
 <script>
+import { ref, onMounted } from "vue";
 import Cookies from "js-cookie";
 export default {
   name: "App",
@@ -28,13 +29,53 @@ export default {
     };
   },
   created() {
-    console.log("VITE_CALLBACK_URL HOME", import.meta.env.VITE_CALLBACK_URL);
+    const param = ref("");
+    const price = ref("");
     const queryParams = this.$route.query;
-    const param = queryParams.param;
-    Cookies.set("param", param, { expires: 7 });
+    console.log("queryParams", queryParams);
 
-    const price = queryParams.price;
-    Cookies.set("price", price, { expires: 7 });
+    // ดึงค่า liff.state จาก queryParams
+    const liffState = queryParams["liff.state"];
+
+    if (liffState) {
+      // ใช้ URLSearchParams แปลง liff.state และดึงค่า param กับ price
+      const queryParamsState = new URLSearchParams(liffState);
+      param.value = queryParamsState.get("param") || "";
+      price.value = queryParamsState.get("price") || "";
+      console.log("param.value", param.value);
+      console.log("price.value", price.value);
+
+      Cookies.set("param", param.value, { expires: 30 });
+
+      Cookies.set("price", price.value, { expires: 30 });
+    }
+
+    // const param_test = ref("");
+    // const price_test = ref("");
+    // สมมติว่า liff.state คือข้อมูล query string ที่ได้รับมา
+    // const liffState = queryParams;
+
+    // ใช้ URLSearchParams ในการแยก query string
+    // const queryParamsTest = new URLSearchParams(liffState);
+    // console.log("queryParamsTest", queryParamsTest);
+
+    // ดึงค่าจาก query string
+    // param_test.value = queryParamsTest.get("param");
+    // price_test.value = queryParamsTest.get("price");
+    // console.log("price_test.value ", price_test.value);
+    // console.log(" param_test.value ", param_test.value);
+
+    //https://fde9-49-49-142-138.ngrok-free.app/?param=free&price=0
+    // console.log("VITE_CALLBACK_URL HOME", import.meta.env.VITE_CALLBACK_URL);
+    // const queryParams = this.$route.query;
+
+    // const param = queryParams.param;
+
+    // Cookies.set("param", param_test.value, { expires: 30 });
+
+    // const price = queryParams.price;
+
+    // Cookies.set("price", price_test.value, { expires: 30 });
   },
   methods: {
     loginWithLine() {
